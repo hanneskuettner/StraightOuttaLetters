b830fbf36e2ddab0ddd9d01e78e497816044d6b7
Hannes Kuettner <hannes.kuettner@gmx.de>
2015-09-19 16:21:31 -0400

    modified song controller

--- Assets/scripts/controller/SongController.cs
+++ Assets/scripts/controller/SongController.cs
@@ -15,19 +15,20 @@ public class SongController : MonoBehaviour {
 	void Start () {
         sourceBattle = GameObject.Find("BattleSource").GetComponent<AudioSource>();
         sourceBeat = GameObject.Find("BeatSource").GetComponent<AudioSource>();
-        currentSong = Song.Load("01-its-tricky");
-        StartAudio();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	}
 
+    public void SetCurrentSong(Song song)
+    {
+        currentSong = song;
+    }
+
     public void StartAudio() {
         sourceBattle.clip = currentSong.battleClip;
         sourceBeat.clip = currentSong.beatClip;
-        sourceBattle.volume = 1.0f;
-        sourceBeat.volume = 1.0f;
         sourceBeat.loop = true;
         sourceBeat.mute = true;
         sourceBattle.mute = false;
