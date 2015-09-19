44c777bc8349684295c2984fca2cffa66369e6ca
marshen <marshen@marshen.de>
2015-09-19 17:05:00 -0400

    scores names and stuff

---
+++ Assets/scripts/ui/UIScore.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIScore : MonoBehaviour {
+    public UnityEngine.UI.Text PlayerScore;
+    public UnityEngine.UI.Text EnemyScore;
+
+    public void SetPlayerScore(int score) {
+        this.PlayerScore.text = score.ToString();
+    }
+
+    public void SetEnemyScore(int score) {
+        this.EnemyScore.text = score.ToString();
+    }
+}
